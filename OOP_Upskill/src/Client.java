252dc514ce77d9f91ae1a26f02c93f4a95f5183b
Travis Defty <travisdefty@gmail.com>
2021-04-23 13:19:42 +0200

    delete files

--- OOP_Upskill/src/Client.java
+++ OOP_Upskill/src/Client.java
@@ -1,9 +1,10 @@
 
-public class Server {
+public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("how are you?");
 	}
 
 }
